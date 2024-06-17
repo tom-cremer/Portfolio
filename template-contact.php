0b4981003d554e25cd9b7d5c75b7f6be7a2f1368
tomcremer <tom.cremer@student.hepl.be>
2024-06-17 16:35:15 +0200

    Re-worked Entire files

--- template-contact.php
+++ template-contact.php
@@ -7,36 +7,37 @@
             <h1><?= get_field('brand_name') ?></h1>
         </div>
     </div>
-    <section class="contact-container">
+    <section class="contact-section">
+        <div class="contact-infos">
+            <ul class="list">
+                <h3>Coordonnées</h3>
+                <?php if (get_field('phone_number')): ?>
+                    <li class="info-item">
+                        <p class="label">Numéro de Téléphone&nbsp;:</p>
+                        <p class="data-phone"><?= get_field('phone_number') ?></p>
+                    </li>
+                <?php endif; ?>
+                <?php if (get_field('mail_adress')): ?>
+                    <li class="info-item">
+                        <p class="label">Adresse Mail&nbsp;:</p>
+                        <a title="Adresse mail de contact" href="mailto:<?= get_field('mail_adress') ?> "
+                           class="data-mail"><?= get_field('mail_adress') ?></a>
+
+                    </li>
+                <?php endif; ?>
+                <?php if (get_field('postal_adress')): ?>
+                    <li class="info-item">
+                        <p class="label">Adresse Postal&nbsp;:</p>
+                        <p class="data-postal"><?= get_field('postal_adress') ?></p>
+                    </li>
+                <?php endif; ?>
+            </ul>
+        </div>
+
         <div class="contact-form">
             <h2 class="contact-title"><?= get_field('contact_title') ?></h2>
             <?= do_shortcode('[contact-form-7 id="e87a582" title="Formulaire de Contact" class:contact-form]'); ?>
         </div>
-        <ul class="contact-infos-list">
-            <h3>Coordonnées</h3>
-            <?php if (get_field('phone_number')): ?>
-                <li class="info-item">
-                    <p class="label">Numéro de Téléphone&nbsp;:</p>
-                    <p class="data-phone"><?= get_field('phone_number') ?></p>
-                </li>
-            <?php endif; ?>
-            <?php if (get_field('mail_adress')): ?>
-                <li class="info-item">
-                    <p class="label">Adresse Mail&nbsp;:</p>
-                    <a title="Adresse mail de contact" href="mailto:<?= get_field('mail_adress') ?> "
-                       class="data-mail"><?= get_field('mail_adress') ?></a>
-
-                </li>
-            <?php endif; ?>
-            <?php if (get_field('postal_adress')): ?>
-                <li class="info-item">
-                    <p class="label">Adresse Postal&nbsp;:</p>
-                    <p class="data-postal"><?= get_field('postal_adress') ?></p>
-                </li>
-            <?php endif; ?>
-
-        </ul>
-
 
     </section>
 
